1dbcead262e28c59202235aec3cd011f7fbcca90
Shehtab Zaman <szaman5@binghamton.edu>
2025-10-07 23:34:50 -0400

    Enable changing tool timeout (#24)
    * Enable changing tool timeout
    
    * Increase sse_read_timeout time as well

--- charge/clients/autogen.py
+++ charge/clients/autogen.py
@@ -58,6 +58,7 @@ class AutoGenClient(Client):
         max_tool_calls: int = 30,
         check_response: bool = False,
         max_multi_turns: int = 100,
+        mcp_timeout: int = 60,
     ):
         """Initializes the AutoGenClient.
 
@@ -83,6 +84,7 @@ class AutoGenClient(Client):
             check_response (bool, optional): Whether to check the response using verifier methods.
                                              Defaults to False (Will be set to True in the future).
             max_multi_turns (int, optional): Maximum number of multi-turn interactions. Defaults to 100.
+            mcp_timeout (int, optional): Timeout in seconds for MCP server responses. Defaults to 60 s.
         Raises:
             ValueError: If neither `server_path` nor `server_url` is provided and MCP servers cannot be generated.
         """
@@ -95,6 +97,7 @@ class AutoGenClient(Client):
         self.max_tool_calls = max_tool_calls
         self.check_response = check_response
         self.max_multi_turns = max_multi_turns
+        self.mcp_timeout = mcp_timeout
 
         if model_client is not None:
             self.model_client = model_client
@@ -139,13 +142,24 @@ class AutoGenClient(Client):
                 if isinstance(server_path, str):
                     server_path = [server_path]
                 for sp in server_path:
-                    self.servers.append(StdioServerParams(command="python3", args=[sp]))
+                    self.servers.append(
+                        StdioServerParams(
+                            command="python3",
+                            args=[sp],
+                            read_timeout_seconds=self.mcp_timeout,
+                        )
+                    )
             if server_url is not None:
                 if isinstance(server_url, str):
                     server_url = [server_url]
                 for su in server_url:
                     self.servers.append(
-                        SseServerParams(url=su, **(server_kwargs or {}))
+                        SseServerParams(
+                            url=su,
+                            timeout=self.mcp_timeout,
+                            sse_read_timeout=self.mcp_timeout,
+                            **(server_kwargs or {}),
+                        )
                     )
 
     @staticmethod
