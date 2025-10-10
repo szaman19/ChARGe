799fad61bd7227f8b19383d60a488465e194c2bc
Shehtab <szaman5@binghamton.edu>
2025-10-10 18:06:15 -0400

    Updated retrosynthesis experiment

--- experiments/Retrosynthesis/reaction_server.py
+++ experiments/Retrosynthesis/reaction_server.py
@@ -5,9 +5,35 @@
 ## SPDX-License-Identifier: Apache-2.0
 ################################################################################
 
+from mcp.server.fastmcp import FastMCP
 from charge.servers.SMARTS_reactions import SMARTS_mcp
+from charge.servers.SMILES_utils import verify_smiles, canonicalize_smiles
+import click
+
+template_free_mcp = FastMCP("template_free_reaction_server")
+
+template_free_mcp.tool()(verify_smiles)
+template_free_mcp.tool()(canonicalize_smiles)
+
 
 if __name__ == "__main__":
-    SMARTS_mcp.run(
-        transport="stdio",
+    click.echo("Starting SMARTS reaction server...")
+
+    @click.command()
+    @click.argument(
+        "--exp_type",
+        default="template",
+        type=click.Choice(["template", "template-free"]),
     )
+    def run_reaction_server(exp_type):
+        if exp_type == "template":
+
+            SMARTS_mcp.run(
+                transport="sse",
+            )
+        elif exp_type == "template-free":
+            template_free_mcp.run(
+                transport="sse",
+            )
+        else:
+            raise ValueError(f"Unknown experiment type: {exp_type}")
