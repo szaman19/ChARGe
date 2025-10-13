443408de0b8d47df11b1881c807f1098b63e3dd0
Shehtab <szaman5@binghamton.edu>
2025-10-12 23:33:50 -0400

    Convert server utils to add arguments in place

--- charge/servers/server_utils.py
+++ charge/servers/server_utils.py
@@ -1,9 +1,15 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
-parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
-parser.add_argument(
-    "--host", type=str, default="http://127.0.0.1", help="Host to run the server on"
-)
-args = parser.parse_args()
 
+def add_server_arguments(parser: argparse.ArgumentParser) -> None:
+    """
+    Add standard server arguments to an argparse parser.
+    Args:
+        parser (argparse.ArgumentParser): The parser to add arguments to.
+    """
+    parser.add_argument(
+        "--port", type=int, default=8000, help="Port to run the server on"
+    )
+    parser.add_argument(
+        "--host", type=str, default="http://127.0.0.1", help="Host to run the server on"
+    )
