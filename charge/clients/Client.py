24b6250544af3eb595faff44f65d0cee71e236a8
Brian C. Van Essen <vanessen1@llnl.gov>
2025-10-30 20:01:54 -0700

    Removed refactored code from Client class.

--- charge/clients/Client.py
+++ charge/clients/Client.py
@@ -122,12 +122,3 @@ class Client:
         parser.add_argument(
             "--history", action="store", type=str, default=".charge-chat-client-history"
         )
-
-    def enable_cmd_history_and_shell_integration(history: str):
-        try:
-            readline.read_history_file(history)
-            readline.set_history_length(1000)
-        except FileNotFoundError:
-            pass
-
-        atexit.register(readline.write_history_file, history)
