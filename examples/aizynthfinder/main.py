371c8404d16edf7b01066da58befea6d0a6a45f9
Shehtab <szaman5@binghamton.edu>
2025-11-24 23:28:28 -0500

    Update aizynthfinder example to use AutoGenPool

--- examples/aizynthfinder/main.py
+++ examples/aizynthfinder/main.py
@@ -2,13 +2,16 @@ import argparse
 import asyncio
 from charge.tasks.Task import Task
 from charge.clients.Client import Client
-from charge.clients.autogen import AutoGenClient
-from charge.servers.AiZynthTools import find_synthesis_routes
+from charge.clients.autogen import AutoGenPool
 from charge.servers.log_progress import LOG_PROGRESS_SYSTEM_PROMPT
 
+
 class AiZynthFinderTask(Task):
-    def __init__(self, lead_molecule: str):
-        system_prompt = "You are a world-class chemist. Your task is to perform retrosynthesis for a target molecule." + LOG_PROGRESS_SYSTEM_PROMPT
+    def __init__(self, lead_molecule: str, **kwargs):
+        system_prompt = (
+            "You are a world-class chemist. Your task is to perform retrosynthesis for a target molecule."
+            + LOG_PROGRESS_SYSTEM_PROMPT
+        )
 
         user_prompt = (
             f"Use available tools to find synthesis routes to make {lead_molecule}\n"
@@ -17,7 +20,7 @@ class AiZynthFinderTask(Task):
             "Consider a few candidates routes and provide your answer in a clear and concise manner. "
         )
 
-        super().__init__(system_prompt=system_prompt, user_prompt=user_prompt)
+        super().__init__(system_prompt=system_prompt, user_prompt=user_prompt, **kwargs)
         self.lead_molecule = lead_molecule
         self.system_prompt = system_prompt
         self.user_prompt = user_prompt
@@ -26,21 +29,23 @@ class AiZynthFinderTask(Task):
 def main():
     # CLI arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lead-molecule", type=str, default="CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
+    parser.add_argument(
+        "--lead-molecule", type=str, default="CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
+    )
     Client.add_std_parser_arguments(parser)
     args = parser.parse_args()
 
-    (model, backend, API_KEY, kwargs) = AutoGenClient.configure(args.model, args.backend)
-    runner = AutoGenClient(
-        task=AiZynthFinderTask(lead_molecule=args.lead_molecule),
-        backend=backend,
-        model=model,
-        api_key=API_KEY,
-        model_kwargs=kwargs,
-        server_url=args.server_urls,
+    agent_pool = AutoGenPool(model=args.model, backend=args.backend)
+
+    task = AiZynthFinderTask(
+        lead_molecule=args.lead_molecule,
+        server_urls=args.server_urls,
+        server_path=args.server_path,
     )
+
+    runner = agent_pool.create_agent(task=task)
     results = asyncio.run(runner.run())
-    print(f"[{model} orchestrated] Task completed. Results: {results}")
+    print(f"[{args.model} orchestrated] Task completed. Results: {results}")
 
 
 if __name__ == "__main__":
