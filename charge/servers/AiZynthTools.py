9af5a829528002de9acbd2505aad7f5cecb281b0
Shehtab <szaman5@binghamton.edu>
2025-11-28 01:28:16 -0500

    Updated retroplanner

--- charge/servers/AiZynthTools.py
+++ charge/servers/AiZynthTools.py
@@ -52,20 +52,34 @@ class Node:
 
 
 class ReactionPath:
-    def __init__(self, route):
+    def __init__(
+        self, route, cur_num_nodes: int = 0, root_parent_node_id: Optional[int] = None
+    ):
+
         self.route = route
         self.nodes: Dict[int, Node] = {}
-        self.num_nodes = 0
+        self.num_nodes = cur_num_nodes
+        self.root_parent_node_id = root_parent_node_id
+
         self._build_path()
-        self.leaf_nodes = [node_id for node_id, node in self.nodes.items() if node.is_leaf]
+        self.leaf_nodes = [
+            node_id for node_id, node in self.nodes.items() if node.is_leaf
+        ]
 
     def _build_path(self):
 
-        self.root = Node(node_id=0, smiles=self.route["smiles"], children=[], is_root=True)
+        self.root = Node(
+            node_id=self.num_nodes,
+            smiles=self.route["smiles"],
+            children=[],
+            is_root=True if self.root_parent_node_id is None else False,
+            parent_id=self.root_parent_node_id,
+        )
         self.nodes[0] = self.root
         self.num_nodes += 1
-        reaction_node = self.route["children"][0]
+
         if "children" in self.route:
+            reaction_node = self.route["children"][0]
             self._add_children(self.root, reaction_node, reaction_node["children"])
 
     def _add_children(self, parent_node, reaction, children):
@@ -86,7 +100,9 @@ class ReactionPath:
                 self.num_nodes += 1
                 if "children" in child:
                     reaction_node = child["children"][0]
-                    self._add_children(child_node, reaction_node, reaction_node["children"])
+                    self._add_children(
+                        child_node, reaction_node, reaction_node["children"]
+                    )
                 else:
                     child_node.is_leaf = True
 
@@ -102,6 +118,7 @@ class ReactionPath:
 
 
 class RetroPlanner:
+
     finder = None
 
     def __init__(self, configfile="config.yml", stock_name="zinc", policy_name="uspto"):
@@ -143,7 +160,9 @@ def is_molecule_synthesizable(smiles: str) -> bool:
         ValueError:  If the molecule is not valid.
     """
     if not HAS_AIZYNTHFINDER:
-        raise ImportError("Please install the aizynthfinder support packages to use this module.")
+        raise ImportError(
+            "Please install the aizynthfinder support packages to use this module."
+        )
 
     logger.info(f"Checking if molecule {smiles} is synthesizable.")
 
@@ -163,7 +182,9 @@ def is_molecule_synthesizable(smiles: str) -> bool:
     for route in routes:
         path = ReactionPath(route=route)
         # check if all leaf nodes are purchasable
-        all_purchasable = all(path.nodes[node_id].purchasable is True for node_id in path.leaf_nodes)
+        all_purchasable = all(
+            path.nodes[node_id].purchasable is True for node_id in path.leaf_nodes
+        )
         if all_purchasable:
             return True
     return False
@@ -181,7 +202,9 @@ def find_synthesis_routes(smiles: str) -> list[dict]:
         ValueError:  If the molecule is not valid.
     """
     if not HAS_AIZYNTHFINDER:
-        raise ImportError("Please install the aizynthfinder support packages to use this module.")
+        raise ImportError(
+            "Please install the aizynthfinder support packages to use this module."
+        )
 
     logger.info(f"Find a synthesis route for molecule {smiles}.")
 
